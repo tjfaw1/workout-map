e68b1823be9ee15cd79ebba79ea31fecbe54d98c
Tim Fawcett <timfawcett21@gmail.com>
2022-07-17 11:09:28 +1000

    Map rendered and GeoLocation stored

--- script.js
+++ script.js
@@ -10,3 +10,37 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// Takes two functions as arguments > Success > Error/Failure
+navigator.geolocation.getCurrentPosition(function (position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  const coords = [latitude, longitude];
+
+  // from Leaflet Map
+  const map = L.map('map').setView(coords, 13);
+  console.log(map);
+  L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+    map.on('click', function(mapEvent) {
+    console.log(mapEvent);
+    L.marker(mapEvent.latlng).addTo(map)
+      .bindPopup(L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+      .setPopupContent('Workout')
+      .openPopup();
+  })
+
+
+}, function () {
+  alert('Could not access your location, please reload page and allow location access to use the application')
+})
